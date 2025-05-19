2fc30a005411b706fc018c1ca3e0a17364beaae6
LuscaAnasta <111717217+LuscaAnasta@users.noreply.github.com>
2025-05-19 00:55:14 -0300

    Add files via upload

--- js/codigo.js
+++ js/codigo.js
@@ -1,14 +1,15 @@
 function exec(e){
     e.preventDefault()
-    
     if(validar()){
         adicionarCoisa();
     }
 
     let x = document.getElementById('lista')
     let a = x.getElementsByTagName('span');
-    console.log(x.rows.length)
-    console.log(a[0].firstChild.textContent)
+    //console.log(x.rows.length)
+    //console.log(a[0].firstChild.textContent)
+
+    mudarEstadoVazio();
     
 }
 
@@ -17,10 +18,6 @@ function validar(){
         tostando("Coisa must be filled...")
         return false;
     }
-
-    
-    
-    
     return true;
 }
 
@@ -37,12 +34,45 @@ function adicionarCoisa() {
     let clon = document.createElement('tr')
     clon.innerHTML = temp;
     document.getElementById('lista').appendChild(clon);
+    alterarValorLista();
     
 
     //let sp = clon.getElementsByTagName('span');
     
     
 }
+function alterarValorLista(){
+
+    let x = document.getElementById("lista");
+    x.value = listaTamanho();
+    console.log(x.value)
+    
+}
+function listaVazia(){
+    let x = document.getElementById('lista');
+    return x.rows.length == 0;
+}
+function listaTamanho(){
+    let x = document.getElementById('lista');
+    return x.rows.length;
+}
+
+//document.getElementById('lista').addEventListener(listaVazia(), mudarEstadoVazio());
+function mudarEstadoVazio(){
+    console.log("aqui 1");
+    let x = document.getElementById('h1coisa');
+    if(listaVazia()){
+        
+        x.className = "show";
+        console.log("aqui 12")
+    }else{
+        x.className = "hidden"
+        console.log("aqui 13")
+    }
+    console.log("aqui 2")
+}
+
+
 
 function getCoisa(){
     let x = document.forms["form-entrada"]["fentrada"].value;
@@ -58,7 +88,7 @@ function tostando(text) {
     var x = document.getElementById("snackbar");
     let sp = x.getElementsByTagName('span');
     sp[0].innerHTML = text ? text : "texto vazio";
-    console.log(x.className)
+    //console.log(x.className)
     if(x.className == "show"){
         showon +=1
         sp[1].innerHTML = ` (${showon})` 
